6e702b829811764f2a0e8b12120dd8d2e4038d00
Samba Siva Reddy <151616423+sambasivareddy-ch@users.noreply.github.com>
2024-06-01 18:28:27 +0000

    Go program added June 01 2024

--- Go/score_of_a_string.go
+++ Go/score_of_a_string.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
-	"string"
+	"math"
 )
 
 func scoreOfString(s string) int {
+	score, string_length := 0, len(s)
 
+	for i := 0; i < string_length-1; i++ {
+		score += int(math.Abs(float64(s[i]) - float64(s[i+1])))
+	}
+
+	return score
+}
+
+func main() {
+	var s string
+	fmt.Print("Enter a string:")
+	fmt.Scan(&s)
+
+	score := scoreOfString(s)
+
+	fmt.Println("Score: ", score)
 }
